9f8dd395f4236225c83da09f5b5cc65cb8146598
braulioACM <barulioacespdesm@gmail.com>
2022-03-03 21:15:01 -0400

    estas son las dos tareas

---
+++ año bisiesto/año bisiesto/año bisiesto/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace año_bisiesto
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 2022;
+            for (int i = 1990; i <= a; i++)
+            {
+                if (i % 4 == 0 && (i % 400 == 0 || i % 100 != 0))
+                    RESULTADO.Items.Add(i);
+            
+            }
+        
+        
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x = Convert.ToInt32(textBox1.Text);
+            if (x % 4 == 0 && (x % 400 == 0 || x % 100 != 0))
+                textBox2.Text = (x + " " + "ES BISIESTO");
+            else
+            {
+                textBox2.Text = (x + " " + "NO ES BISIESTO");
+            }
+        }
+    }
+}
