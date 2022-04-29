6b3af9fc0b9a04525614827fe94a62770d85b04e
Braulio190204 <98610866+Braulio190204@users.noreply.github.com>
2022-04-29 10:17:25 -0400

    agregar refrescos

---
+++ agregar y eliminar refresco/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace agregar_y_eliminar_refresco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.txtagregar = new System.Windows.Forms.TextBox();
+            this.listrefrescos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(309, 49);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(100, 24);
+            this.btnagregar.TabIndex = 0;
+            this.btnagregar.Text = "agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(637, 94);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(100, 24);
+            this.btneliminar.TabIndex = 1;
+            this.btneliminar.Text = "eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // txtagregar
+            // 
+            this.txtagregar.Location = new System.Drawing.Point(291, 79);
+            this.txtagregar.Multiline = true;
+            this.txtagregar.Name = "txtagregar";
+            this.txtagregar.Size = new System.Drawing.Size(141, 27);
+            this.txtagregar.TabIndex = 2;
+            // 
+            // listrefrescos
+            // 
+            this.listrefrescos.FormattingEnabled = true;
+            this.listrefrescos.Location = new System.Drawing.Point(602, 134);
+            this.listrefrescos.Name = "listrefrescos";
+            this.listrefrescos.Size = new System.Drawing.Size(135, 173);
+            this.listrefrescos.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listrefrescos);
+            this.Controls.Add(this.txtagregar);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnagregar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.TextBox txtagregar;
+        private System.Windows.Forms.ListBox listrefrescos;
+    }
+}
+
---
+++ agregar y eliminar refresco/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace agregar_y_eliminar_refresco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            var a = txtagregar.Text;
+            listrefrescos.Items.Add(a);
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            listrefrescos.Items.Remove(listrefrescos.SelectedItem);
+        }
+    }
+}
