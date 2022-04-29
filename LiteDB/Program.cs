ac63244ca18ba5c53d9d9f5a16d994c722beec85
Braulio190204 <98610866+Braulio190204@users.noreply.github.com>
2022-04-29 10:39:16 -0400

    LiteDB

---
+++ LiteDB/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LiteDB
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string[] matriz = new string[30];
+
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("dame los 2 numeros");
+                matriz[i] = Console.ReadLine();
+
+
+            }
+
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("estos son los numeros");
+                Console.WriteLine(matriz[i]);
+
+
+            }
+
+        }
+    }
+}
